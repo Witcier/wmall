13aa2ec9ea854c48d26ec1b31ee083d5b2f59a30
vagrant <vagrant@homestead>
2020-09-28 07:37:26 +0000

    用户界面 --商品搜索

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -9,10 +9,40 @@ class ProductsController extends Controller
 {
     public function index(Request $request)
     {
-        $products = Product::query()->where('status',true)->paginate(16);
+        //创建查询构造器
+        $builder = Product::query()->where('status',true);
+
+        //search 参数用来模糊搜索商品
+        if ($search = $request->input('search','')) {
+            $like = '%'.$search.'%';
+
+            $builder->where(function ($query) use ($like) {
+                $query->where('title','like',$like)
+                    ->orWhere('description','like','$like')
+                    ->orWhereHas('skus',function ($query) use ($like) {
+                        $query->where('title','like',$like)
+                            ->orWhere('description','like',$like);
+                    });
+            });
+        }
+
+        // order 参数控制商品的排序规则
+        if ($order = $request->input('order','')) {
+            if (preg_match('/^(.+)_(asc|desc)$/',$order,$m)) {
+                if (in_array($m[1],['price','sold_count','rating'])) {
+                    $builder->orderBy($m[1],$m[2]);
+                }
+            }
+        }
+
+        $products = $builder->paginate(16);
 
         return view('products.index',[
             'products' => $products,
+            'filters' =>[
+                'search' => $search,
+                'order' => $order,
+            ],
         ]);
     }
 }
