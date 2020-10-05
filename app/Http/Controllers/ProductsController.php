c7299e21df313faf9c2c65feb9075fb38b350887
Witcier <770201276@qq.com>
2020-10-05 07:26:17 +0000

    异常处理

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\InvalidRequestException;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -50,7 +51,7 @@ class ProductsController extends Controller
     {
         //判断商品是否上架
         if (!$product->status) {
-            throw new \Exception('商品未上架');
+            throw new InvalidRequestException('商品未上架');
         }
 
         return view('products.show',[
