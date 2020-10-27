554a6c302199157feb5794d3c84e422375507fa6
Witcier <770201276@qq.com>
2020-10-27 07:33:07 +0000

    众筹模型

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,6 +8,14 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
+    const TYPE_NORMAL = 'normal';
+    const TYPE_CROWDFUNDING = 'crowdfunding';
+
+    public static $typeMap = [
+        self::TYPE_NORMAL => '普通商品',
+        self::TYPE_CROWDFUNDING => '众筹商品',
+    ];
+
     protected $fillable = [
         'title',
         'description',
@@ -17,6 +25,7 @@ class Product extends Model
         'sold_count',
         'review_count',
         'price',
+        'type',
     ];
 
     protected $casts = [
@@ -41,5 +50,10 @@ class Product extends Model
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function crowdfunding()
+    {
+        return $this->hasOne(CrowdfundingProduct::class);
     }
 }
