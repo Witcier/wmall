461afdbc96e9009c0e0c5d0255e9231daab815a6
Witicer <770201276@qq.com>
2020-12-22 09:01:20 +0000

    修复不能生成假数据

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,9 +15,9 @@ class DatabaseSeeder extends Seeder
 
         $this->call(UsersSeeder::class);
         $this->call(UserAddressesSeeder::class);
-        // $this->call(CategoriesSeeder::class);
-        // $this->call(ProductsSeeder::class);
-        // $this->call(CouponCodesSeeder::class);
-        // $this->call(OrdersSeeder::class);
+        $this->call(CategoriesSeeder::class);
+        $this->call(ProductsSeeder::class);
+        $this->call(CouponCodesSeeder::class);
+        $this->call(OrdersSeeder::class);
     }
 }
