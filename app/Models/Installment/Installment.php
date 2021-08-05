bb710355dad66f3864da02bf28101ff2ea28d49f
Witcier <770201276@qq.com>
2021-08-05 15:22:42 +0800

    分期付款模型

---
+++ app/Models/Installment/Installment.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Models\Installment;
+
+use App\Models\Order\Order;
+use App\Models\User\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Installment extends Model
+{
+    use HasFactory;
+
+    const STATUS_PENDING = 1;
+    const STATUS_REPAYING = 2;
+    CONST STATUS_FINISHED = 3;
+
+    public static $statusMap = [
+        self::STATUS_PENDING => '未执行',
+        self::STATUS_REPAYING => '还款中',
+        self::STATUS_FINISHED => '还款完成',
+    ];
+
+    protected $fillable = [
+        'no', 'total_amount', 'count', 'fee_rate', 'fine_rate', 'status'
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (!$model->no) {
+                $model->no = static::findAvailableNo();
+
+                if (!$model->no) {
+                    return false;
+                }
+            }
+        });
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function items()
+    {
+        return $this->hasMany(Item::class);
+    }
+
+    public static function findAvailableNo()
+    {
+        $prefix = date('YmdHis');
+        for ($i = 0; $i < 10; $i++) {
+            $no = $prefix . str_pad(random_int(0, 999999), 6, '0', STR_PAD_LEFT);
+
+            if (!static::query()->where('no', $no)->exists()) {
+                return $no;
+            }
+        }
+
+        \Log::warning("find installment no failed");
+
+        return false;
+    }
+}
---
+++ app/Models/Installment/Item.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models\Installment;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    use HasFactory;
+
+    const REFUND_STATUS_PENDING = 1;
+    const REFUND_STATUS_PROCESSING = 2;
+    const REFUND_STATUS_SUCCESS = 3;
+    const REFUND_STATUS_FAILED = 4;
+
+    public static $refundStatusMap = [
+        self::REFUND_STATUS_PENDING => '未退款',
+        self::REFUND_STATUS_PROCESSING => '退款中',
+        self::REFUND_STATUS_SUCCESS => '退款成功',
+        self::REFUND_STATUS_FAILED => '退款失败',
+    ];
+
+    protected $fillable = [
+        'sequence', 'base', 'fee', 'fine', 'due_at', 'paid', 'paid_at', 'payment_method', 'payment_no', 'refund_status',
+    ];
+
+    protected $dates = [
+        'due_at', 'paid_at',
+    ];
+
+    public function installment()
+    {
+        return $this->belongsTo(Installment::class);
+    }
+
+    public function getTotalAttribute()
+    {
+        $total = big_number($this->base)->add($this->fee);
+        if (!is_null($this->fine)) {
+            $total->add($this->fine);
+        }
+
+        return $total->getValue();
+    }
+
+    public function getIsOverDueAttribute()
+    {
+        return Carbon::now()->gt($this->due_at);
+    }
+}
